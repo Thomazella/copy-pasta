5eccd939fd70faef1d40dd7bb45c66088f9e7e5e
Raphael Thomazella <thomazella9@gmail.com>
2020-05-08 12:37:18 -0300

    chore(misc): add missing moneyRound test

--- utils/__test__/moneyRound.spec.ts
+++ utils/__test__/moneyRound.spec.ts
@@ -1,16 +1,24 @@
-export const moneyRound = (input: number): number => {
-  let result: number;
-  if ((input !== 0 && !input) || Number.isNaN(input)) {
-    // eslint-disable-next-line no-console
-    console.warn('Invalid input to MoneyRound: ', input);
-    return 0;
-  }
+import { moneyRound } from '../moneyRound';
 
-  const decimals = input.toString().replace(/^[^.]+[.]/, '').length;
-  if (decimals <= 2) {
-    return input;
-  }
-  result = Math.fround(input); // round to nearest 32bits float
-  result = Math.trunc(result * 100) / 100; // 2 decimals
-  return result;
-};
+describe('behavior', () => {
+  test('rounds a number to 2 decimals', () => {
+    expect(moneyRound(0.234)).toEqual(0.23);
+  });
+  test('rounds a long float to float32 and 2 decimals', () => {
+    expect(moneyRound(0.2834729387492743)).toEqual(0.28);
+  });
+  test('doenst round a number that is aready 2 decimals', () => {
+    expect(moneyRound(0.28)).toEqual(0.28);
+  });
+  test('doenst round a number that is aready 1 decimals', () => {
+    expect(moneyRound(0.4)).toEqual(0.4);
+  });
+  test('third decimal is ignored regardless of being high or low', () => {
+    expect(moneyRound(0.999)).toEqual(0.99);
+    expect(moneyRound(0.991)).toEqual(0.99);
+  });
+  test('returns 0 if input is invalid', () => {
+    expect(moneyRound()).toEqual(0);
+    expect(moneyRound(NaN)).toEqual(0);
+  });
+});
